fc489601150347b79befd21327135d4bd938c5b9
WaltGurley <walt.gurley@gmail.com>
2021-01-04 10:49:03 -0500

    updated post 2020 symposium

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,17 +4,6 @@
 
     <h1 class="ml-8 sm:mx-32 text-3xl text-black text-center">Southeast Data Librarian Symposium</h1>
 
-    <CardAnnouncement>
-      <div class="md:ml-1 p-2 border-t-2 md:border-t-0 md:border-l-2 border-red-600">
-        <p>
-        The Southeast Data Librarian Symposium (SEDLS) is returning virtually in 2020!
-        </p>
-        <p class="mt-4">
-          Visit the <a class="text-red-600 transition duration-200 ease-in-out hover:text-blue-600" href="https://se-datalibrarian.github.io/2020/">SEDLS 2020 website</a> for more information.
-        </p>
-      </div>
-    </CardAnnouncement>
-
     <h2 class="text-2xl mt-4">About</h2>
     <p class="mt-4">
       The Southeast Data Librarian Symposium is intended to provide a low-cost opportunity for librarians and other research data specialists to gather and explore developments in the field of data librarianship, including the management and sharing of research data. Check out the past symposium websites below for an idea of programming content.
@@ -27,6 +16,7 @@
     <ul class="mt-4 ml-4 list-disc list-inside">
       <li><a class="text-red-600 transition duration-200 ease-in-out hover:text-blue-600" href="https://se-datalibrarian.github.io/2018/">SEDLS 2018: The Georgia Tech Library in Atlanta, GA</a></li>
       <li><a class="text-red-600 transition duration-200 ease-in-out hover:text-blue-600" href="https://se-datalibrarian.github.io/2019/">SEDLS 2019: Tulane University in New Orleans, LA</a></li>
+      <li><a class="text-red-600 transition duration-200 ease-in-out hover:text-blue-600" href="https://se-datalibrarian.github.io/2020/">SEDLS 2020: Online</a></li>
     </ul>
   </Layout>
 </template>
