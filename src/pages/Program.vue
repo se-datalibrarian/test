acd989b1feccfc5e6dadd0237b4bdd71cee4e2d0
WaltGurley <walt.gurley@gmail.com>
2021-07-13 15:03:02 -0400

    updated program page content order

--- src/pages/Program.vue
+++ src/pages/Program.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <h1 class="text-3xl">Program</h1>
-    <p class="mt-4">Submit a proposal and help create the 2021 program!</p>
+    <p class="mt-4">Submit a proposal, and help create the 2021 program!</p>
     <ButtonLinkDefault :url="'#'" class="block mx-auto mt-4 w-max">
       Submit a Proposal
     </ButtonLinkDefault>
@@ -9,10 +9,7 @@
       The Southeast Data Librarian Symposium is soliciting for virtual workshops, short talks, and poster presentations for the 2021 program.
     </p>
     <p class="mt-4">
-      Check out <a class="link" href="https://se-datalibrarian.github.io/2020/program/">last year's program</a> to view abstracts, posters, and presentation recordings for an idea of content.
-    </p>
-    <p class="mt-4">
-      Topics of particular interest this year include:
+      Topics of particular interest this year include, but are not limited to:
     </p>
     <ul class="mt-4 list-disc list-inside">
       <li class="ml-8">Using available tools for research data management</li>
@@ -20,6 +17,9 @@
       <li class="ml-8">Data support for student research</li>
       <li class="ml-8">Addressing data literacy in the curriculum</li>
     </ul>
+    <p class="mt-4">
+      Check out <a class="link" href="https://se-datalibrarian.github.io/2020/program/">last year's program</a> to view abstracts, posters, and presentation recordings for an idea of content.
+    </p>
   </Layout>
 </template>
 
