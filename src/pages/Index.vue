2ce35783238b289407564f20c634971d74b8fb5d
WaltGurley <walt.gurley@gmail.com>
2021-08-25 06:45:48 -0400

    removed call for proposals language

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,20 +4,20 @@
 
     <h1 class="w-full text-center text-2xl">Southeast Data Librarian Symposium 2021</h1>
     <h2 class="w-full mt-2 text-center text-xl">Online, October 13-15</h2>
-    <CardAnnouncement>
-      <h2 class="text-lg">The call for proposals is open now through August 13!</h2>
+    <!-- <CardAnnouncement>
+      <h2 class="text-lg">Registration is open for SEDLS 2021!</h2>
       <p class="mt-4">
-        Visit the <g-link class="link" to="/program">Program Page</g-link> for more information or use the button below to access the proposal submission form.
+        Visit the <g-link class="link" to="/registration">Registration Page</g-link> for more information or use the button below to register.
       </p>
-      <ButtonLinkDefault :url="'https://forms.gle/cHx569XJUwAhTokZA'" class="mx-auto mt-4 mb-2">
-        Submit a Proposal
+      <ButtonLinkDefault :url="'#'" class="mx-auto mt-4 mb-2">
+        Register for SEDLS 2021
       </ButtonLinkDefault>
-    </CardAnnouncement>
+    </CardAnnouncement> -->
     <p class="mt-4">
       The Southeast Data Librarian Symposium (SEDLS) is returning online in 2021! The program will consist of workshops, short presentations, and networking opportunities October 13-15, 2021.
     </p>
     <p class="mt-4">
-      Check out <a class="link" href="https://se-datalibrarian.github.io/2020/program/">last year's program</a> to view abstracts, posters, and presentation recordings for an idea of content.
+      The SEDLS planning committee is currently putting together the 2021 program. Check out <a class="link" href="https://se-datalibrarian.github.io/2020/program/">last year's program</a> to view abstracts, posters, and presentation recordings for an idea of content.
     </p>
     <p class="mt-4">
       SEDLS is open to all who wish to attend, including students, data managers, and data scientists. Learn more about SEDLS on the <g-link class="link" to="/about">About Us page</g-link>.
--- src/pages/Program.vue
+++ src/pages/Program.vue
@@ -1,22 +1,9 @@
 <template>
   <Layout>
     <h1 class="text-3xl">Program</h1>
-    <p class="mt-4 italic">Submit a proposal, and help create the 2021 program!</p>
-    <ButtonLinkDefault :url="'https://forms.gle/cHx569XJUwAhTokZA'" class="block mx-auto mt-4 w-max">
-      Submit a Proposal
-    </ButtonLinkDefault>
     <p class="mt-4">
-      The Southeast Data Librarian Symposium is soliciting for virtual workshops, short talks, and poster presentations for the 2021 program. <strong>The deadline for proposals is Friday, August 13, 2021.</strong>
+      The call for proposals for the Southeast Data Librarian Symposium has ended. The planning committee is in the process of developing the 2021 program.
     </p>
-    <p class="mt-4">
-      Topics of particular interest this year include, but are not limited to:
-    </p>
-    <ul class="mt-4 list-disc list-inside">
-      <li class="ml-8">Using available tools for research data management</li>
-      <li class="ml-8">Using data analysis and data visualization tools</li>
-      <li class="ml-8">Data support for student research</li>
-      <li class="ml-8">Addressing data literacy in the curriculum</li>
-    </ul>
     <p class="mt-4">
       Check out <a class="link" href="https://se-datalibrarian.github.io/2020/program/">last year's program</a> to view abstracts, posters, and presentation recordings for an idea of content.
     </p>
