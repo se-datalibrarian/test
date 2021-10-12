cc52e30fc8ee1ed6d83c660f8dd8c8db942fac38
WaltGurley <walt.gurley@gmail.com>
2021-10-12 15:50:21 -0400

    added links to coded of conduct violation form

--- src/pages/CodeOfConduct.vue
+++ src/pages/CodeOfConduct.vue
@@ -2,9 +2,9 @@
   <Layout>
     <h1 class="text-3xl">Code of Conduct</h1>
     <p class="mt-4">
-      The Southeast Data Librarian Symposium organizers are committed to providing an environment that allows attendees to participate during the day without fear of harassment or discriminatory behavior. Due to the nature of this symposium being completely virtual we expect attendees to follow this code of conduct. We will use a Google Form that is actively monitored by symposium organizers for reporting any violations of the Code of Conduct.
+      The Southeast Data Librarian Symposium organizers are committed to providing an environment that allows attendees to participate during the day without fear of harassment or discriminatory behavior. Due to the nature of this symposium being completely virtual we expect attendees to follow this code of conduct. We use a Google Form that is actively monitored by symposium organizers for reporting any violations of the Code of Conduct. Use the button below to access the form and report a violation.
     </p>
-    <!-- <a class="block my-8 text-center" href="https://forms.gle/vU65Dkvi5kHyfdYe7"><ButtonLinkDefault>Report a violation</ButtonLinkDefault></a> -->
+    <a class="block my-8 text-center" href="https://forms.gle/vU65Dkvi5kHyfdYe7"><ButtonLinkDefault>Report a violation</ButtonLinkDefault></a>
     <h2 class="mt-4 text-2xl">Unacceptable Behavior</h2>
     <p class="mt-4">
       <span class="font-bold">Abusive Behavior:</span> Harassment is understood as any behavior that threatens or demeans another person or group, or produces an unsafe environment. It includes offensive verbal comments or non-verbal expressions related to gender, gender identity, gender expression, sexual orientation, disability, physical appearance, body size, race, age, religious or political beliefs; sexual or discriminatory images in online spaces; deliberate intimidation, stalking, following, harassing photography, screenshots or recording; sustained disruption of talks or other events, and unwelcome sexual attention.
@@ -20,11 +20,8 @@
       If any of the symposium organizers receive reports of any violations to the above listings you will be removed from the meeting for the duration of the symposium. You will be blocked from returning to any of the sessions that happen throughout the symposium and will be removed from the Discord server.
     </p>
     <p class="mt-4">
-      During the symposium, a link to a Google Form will be provided on this page for reporting code of conduct violations. Reports will be kept confidential to as small a group as reasonably possible.
-    </p>
-    <!-- <p class="mt-4">
       Code of conduct violations can be reported through the <a class="link" href="https://forms.gle/vU65Dkvi5kHyfdYe7">Report Code of Conduct Violation Google Form</a>. Reports will be kept confidential to as small a group as reasonably possible.
-    </p> -->
+    </p>
     <p class="mt-4">
       Symposium organizers will react to the incident as soon as possible and make a decision. Once a decision is made we will communicate to the alleged harasser about the complaint and the actions that will happen. We will then follow up with the accuser about what actions were taken and check in with them to see how they are and what additional support they need.
     </p>
