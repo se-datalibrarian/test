5ca7eced72a94725f2197421c881a278df5b3185
Walt Gurley <walt.gurley@gmail.com>
2021-11-03 16:18:43 -0400

    added info on program planning

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,10 +2,17 @@
   <Layout>
     <h1 class="text-3xl">About</h1>
     <p class="mt-4">
-      The Southeast Data Librarian Symposium is intended to provide an opportunity for librarians and other research data specialists to explore developments in the field of data librarianship, including the management and sharing of research data.
+      The Southeast Data Librarian Symposium (SEDLS) is intended to provide an opportunity for librarians and other research data specialists to explore developments in the field of data librarianship, including the management and sharing of research data.
     </p>
     <p class="mt-4">
       In addition to learning about new work in the field, attendees will have the opportunity to network and build partnerships with regional colleagues. It is open to all who wish to attend, including students, data managers, and data scientists.
+    </p>
+    <h2 class="text-2xl mt-4">Program Planning</h2>
+    <p>
+      The symposium is organized by a volunteer committee of librarians working in the field of data librarianship. Symposium themes are based on responses from an interest survey provided to previous SEDLS attendees and relevant data librarianship topics identified by the planning committee.
+    </p>
+    <p>
+      The program is developed based on selected proposals submitted by the community. All proposal abstracts are peer reviewed by the planning committee under a single-blind review protocol blind to author and institution.
     </p>
     <h2 class="text-2xl mt-4">Planning committee</h2>
     <div class="container sm:w-11/12 mx-auto grid sm:grid-cols-2">
